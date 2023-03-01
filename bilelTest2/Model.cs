fbf2f1a50109cff78deb841dabd5ce9688573636
riadh chebbi <supremespiders@gmail.com>
2023-03-01 19:21:12 +0100

    Initial commit

---
+++ bilelTest2/Model.cs
@@ -0,0 +1,26 @@
+﻿namespace bilelTest2;
+
+public class Model
+{
+    public ReleaseStatusSection[] releaseStatusSection { get; set; } 
+
+public class ReleaseStatusSection
+{
+    public CarrierStatus carrierStatus { get; set; }
+    public CustomStatus customStatus { get; set; }
+}
+
+public class CarrierStatus
+{
+    public string status { get; set; }
+    public string date { get; set; }
+}
+
+public class CustomStatus
+{
+    public string status { get; set; }
+    public string date { get; set; }
+}
+
+
+}
---
+++ bilelTest2/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Text.Json.Serialization;
+using bilelTest2;
+using Newtonsoft.Json;
+
+var model = new Model()
+{
+    releaseStatusSection = new[]
+    {
+        new Model.ReleaseStatusSection()
+        {
+            carrierStatus = new Model.CarrierStatus() { status = "s", date = "d" },
+            customStatus = new Model.CustomStatus() { status = "as", date = "a" }
+        }
+    }
+};
+Console.WriteLine(JsonConvert.SerializeObject(model,Formatting.Indented));
+
+Console.ReadLine();
